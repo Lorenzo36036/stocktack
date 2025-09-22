98153f6f7888d740af7b68b729ed52491431ff84
Lorenzo Parra <alejandro36036@gmail.com>
2025-09-22 19:02:16 -0400

    feature: implementation creation routes for login and register
    Routes were created for registration and login, and the functionality to create users was implemented in the service. The hash still needs to be implemented.

--- backend/src/module/auth/auth.controller.ts
+++ backend/src/module/auth/auth.controller.ts
@@ -1,15 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { CreateUserDto, UpdateUserDto } from './dto/index';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
+  @Post('register')
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.authService.createUser(createUserDto);
+  }
+
+  @Post('login')
+  sigIn(@Body() createUserDto: CreateUserDto) {
+    return this.authService.singIn(createUserDto);
   }
 
   @Get()
@@ -23,8 +35,8 @@ export class AuthController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.authService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
--- backend/src/module/auth/auth.service.ts
+++ backend/src/module/auth/auth.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { CreateUserDto, UpdateUserDto, LoginUserDto } from './dto/index';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+  async createUser(createAuthDto: CreateUserDto) {
+    try {
+      await this.userRepository.save(createAuthDto);
+      return createAuthDto;
+    } catch (error) {
+      console.log(error);
+    }
   }
-
+  singIn(createAuthDto: LoginUserDto) {
+    return createAuthDto;
+  }
   findAll() {
     return `This action returns all auth`;
   }
@@ -16,8 +29,8 @@ export class AuthService {
     return `This action returns a #${id} auth`;
   }
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return updateUserDto;
   }
 
   remove(id: number) {
