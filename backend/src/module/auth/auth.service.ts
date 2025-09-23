c2cd4285c06e88452d759ab972f53b54019b4cb2
Lorenzo Parra <alejandro36036@gmail.com>
2025-09-23 18:28:45 -0400

    feature:  Implementation of login
    creation of token the login and autherization request error

--- backend/src/module/auth/auth.service.ts
+++ backend/src/module/auth/auth.service.ts
@@ -1,26 +1,65 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateUserDto, UpdateUserDto, LoginUserDto } from './dto/index';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService,
   ) {}
   async createUser(createAuthDto: CreateUserDto) {
     try {
+      createAuthDto.password = await bcrypt.hash(createAuthDto.password, 10);
       await this.userRepository.save(createAuthDto);
       return createAuthDto;
     } catch (error) {
       console.log(error);
     }
   }
-  singIn(createAuthDto: LoginUserDto) {
-    return createAuthDto;
+  async singIn(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: {
+        uuid: true,
+        username: true,
+        email: true,
+        password: true,
+      },
+    });
+
+    if (!user) throw new BadRequestException('user not found');
+
+    if (!this.verificationLogin(password, user.password))
+      throw new UnauthorizedException('password incorrect');
+
+    const payload = { uuid: user.uuid };
+    return {
+      token: this.jwtService.sign(payload),
+      user: {
+        uuid: user.uuid,
+        username: user.username,
+        email: user.email,
+      },
+    };
   }
+
+  verificationLogin(password: string, hash: string) {
+    const isMatch = bcrypt.compareSync(password, hash);
+    return isMatch;
+  }
+
   findAll() {
     return `This action returns all auth`;
   }
