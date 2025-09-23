3eaf5d13179bfc73eef07d697e2ee10b90246556
Lorenzo Parra <alejandro36036@gmail.com>
2025-09-23 18:26:15 -0400

    feature: abjust of entity
    Important entities are hidden in automatic queries

--- backend/src/module/auth/entities/user.entity.ts
+++ backend/src/module/auth/entities/user.entity.ts
@@ -1,18 +1,20 @@
 import { Column, Entity } from 'typeorm';
 import { PrimaryGeneratedColumn } from 'typeorm';
 import { userRoles } from '../../../common/utils/enum';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn('uuid')
   uuid: string;
-  @Column()
+  @Column({ unique: true })
   username: string;
-  @Column()
+  @Column({ select: false })
   password: string;
-  @Column()
+  @Column({ unique: true })
   email: string;
-  @Column({ default: true })
+
+  @Column({ default: true, select: false })
   isActive: boolean;
   @Column({ type: 'enum', enum: userRoles, default: 'user' })
   rol: userRoles;
