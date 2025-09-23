ca35d2d558d3d0f9ca9c2112d654a176d2649778
Lorenzo Parra <alejandro36036@gmail.com>
2025-09-23 18:27:38 -0400

    feature: import of jwt token
    import in auth.module the jwt for generate tokens

--- backend/src/module/auth/auth.module.ts
+++ backend/src/module/auth/auth.module.ts
@@ -3,9 +3,17 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      global: true,
+      secret: 'secretKey',
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [AuthService],
