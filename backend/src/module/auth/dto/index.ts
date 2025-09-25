509a7804d847b9bc21a9aa6d4d0545e7d656dc47
Lorenzo Parra <alejandro36036@gmail.com>
2025-09-25 17:52:30 -0400

    Feature:  deleting files to move them to other folders
    deleting files to move them to other folders

--- backend/src/module/auth/dto/index.ts
+++
@@ -1,3 +0,0 @@
-export { UpdateUserDto } from './update-user-auth.dto';
-export { CreateUserDto } from './create-user-auth.dto';
-export { LoginUserDto } from './login-user-auth.dto';
--- backend/src/module/auth/dto/update-user-auth.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user-auth.dto';
-
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
