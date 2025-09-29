50927a2b3d154624ebcfee6d35c9ff0acd031999
Lorenzo Parra <alejandro36036@gmail.com>
2025-09-29 16:39:30 -0400

    feature: creation  dto of create users and update
    creation  dto of users

---
+++ backend/src/module/user/dto/create-user-auth.dto.ts
@@ -0,0 +1,30 @@
+import { userRoles } from '@/common/utils/enum';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: 'username is required' })
+  @MinLength(4, { message: 'username required minimum 4 characters' })
+  @IsString()
+  username: string;
+  @IsNotEmpty({ message: 'password is required' })
+  @MinLength(4, { message: 'password required minimum 4 characters' })
+  @IsString()
+  password: string;
+  @IsEmail()
+  @IsNotEmpty({ message: 'email is required' })
+  email: string;
+
+  @IsOptional()
+  @IsEnum(userRoles, {
+    each: true,
+    message: 'rol must be a valid user role [user, admin]',
+  })
+  roles?: userRoles[];
+}
---
+++ backend/src/module/user/dto/index.ts
@@ -0,0 +1,2 @@
+export { UpdateUserDto } from './update-user-auth.dto';
+export { CreateUserDto } from './create-user-auth.dto';
