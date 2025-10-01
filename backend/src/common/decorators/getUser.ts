86442624131a3583356d8baffae2a507c211729a
Lorenzo Parra <alejandro36036@gmail.com>
2025-09-30 20:37:17 -0400

    feature: decorators custom
    get user of tokens

---
+++ backend/src/common/decorators/getUser.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+// src/common/decorators/user.decorator.ts
+
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const GetUser = createParamDecorator(
+  (data: string | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const user = request.user;
+
+    if (data) {
+      return user?.[data];
+    }
+
+    return user;
+  },
+);
